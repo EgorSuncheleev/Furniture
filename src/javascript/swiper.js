c8b66cecba1db0b11939bb53f402e55461674d19
Egor Suncheleev <suncheleev.egor@mail.ru>
2025-10-26 18:12:09 +0400

    Свайпер

--- src/javascript/swiper.js
+++ src/javascript/swiper.js
@@ -1,1 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+});
 
+const element = document.querySelector(".swiper-button-prev");
+element.remove();
+
+const del = document.querySelector(".swiper-button-next");
+del.remove();
